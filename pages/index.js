f8d0ecf8cb889d7cc490614b43b4a56953156d70
AndresGav <andresgavino11@hotmail.com>
2021-04-30 18:17:15 -0500

    Fetch Api Rick Morty

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,26 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const defaultEndPoint = 'https://rickandmortyapi.com/api/character'
+
+export async function getServerSideProps(){
+  
+  const res = await fetch(defaultEndPoint);
+  const data = await res.json();
+
+  return{
+    props:{
+      data
+    }
+  }
+}
+
+export default function Home({data}) {
+
+  //console.log('data:', data )
+  const {results = []}  = data;
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,45 +30,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      
+
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Rick And Morty
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
+        <div style={{width:'100%', height:'200px', backgroundColor:'#000'}}></div>
+        
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          {results.map(result =>{
+            const{id, name}=result;
+            return(
+              <div className={styles.card}>
+                <h2>{name}</h2>
+              </div>
+            )
+          }
+          )}
+          
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
+
       </main>
 
       <footer className={styles.footer}>
